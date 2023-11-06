17db42a0d792735ed65bb74e96a6325f881d0854
Petar Dimanov <pkdimanov@gmail.com>
2023-11-06 11:55:32 +0200

    install Laravel Breeze with Vue and Tailwind

---
+++ resources/js/Pages/Home/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <Head title="Home" />
+
+    <GuestLayout>
+        Home
+    </GuestLayout>
+</template>
+
+<script setup>
+import {Head} from "@inertiajs/vue3";
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+</script>
+
+<style scoped>
+
+</style>
